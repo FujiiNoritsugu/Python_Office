3fbb614a91b2e96111789f3ac5ced8bd6ffe5f2c
Fujii Noritsugu <quercus491@gmail.com>
2022-05-18 06:27:47 +0900

    Excelの演習の解答

--- test_excel.py
+++ test_excel.py
@@ -1,3 +1,4 @@
+import datetime
 from openpyxl import Workbook
 wb = Workbook()
 
@@ -11,8 +12,13 @@ ws['A1'] = 42
 ws.append([1, 2, 3])
 
 # Python types will automatically be converted
-import datetime
 ws['A2'] = datetime.datetime.now()
 
+ws_new = wb.create_sheet("追加シート")
+value_data = 0
+for x in range(1, 11):
+    for y in range(1, 11):
+        value_data = 10 * (x - 1) + y
+        ws_new.cell(row=x, column=y).value = value_data
 # Save the file
 wb.save("sample.xlsx")
