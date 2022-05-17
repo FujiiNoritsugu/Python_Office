9ae6b95c8c39f4ffaa2ded607194f68be92fb1e8
Fujii Noritsugu <quercus491@gmail.com>
2022-05-18 05:43:36 +0900

    パワーポイントの演習の解答

--- test_power.py
+++ test_power.py
@@ -1,12 +1,23 @@
 from pptx import Presentation
 
 prs = Presentation()
-title_slide_layout = prs.slide_layouts[0]
+title_slide_layout = prs.slide_layouts[3]
 slide = prs.slides.add_slide(title_slide_layout)
 title = slide.shapes.title
 subtitle = slide.placeholders[1]
 
-title.text = "Hello, World!"
-subtitle.text = "python-pptx was here!"
+title.text = "変更後のパワーポイントファイル"
+subtitle.text = "レイアウトを変更するとどうなる？"
 
-prs.save('test.pptx')
+count = 0
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                count += len(run.text)
+
+print(f'パワーポイント内の文字数は{count}個です。')
+
+prs.save('test2.pptx')
